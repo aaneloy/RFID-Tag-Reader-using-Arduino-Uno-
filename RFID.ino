1180aed0dae29f57eb42bb7b7e6267b89403ee99
Asif Ahmed Neloy <asif.neloy@northsouth.edu>
2018-03-09 20:58:43 +0600

    Add files via upload

---
+++ RFID.ino
@@ -0,0 +1,64 @@
+/*
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno           Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ * 
+ * 8A 69 73 5B  CB 08 04 00  62 63 64 65  66 67 68 69
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN     9       // 
+#define SS_PIN      10      //
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+void setup() {
+    Serial.begin(9600);     // Initialize serial communications with the PC
+    while (!Serial);        // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+    SPI.begin();            // Init SPI bus
+    mfrc522.PCD_Init();     // Init MFRC522
+    ShowReaderDetails();    // Show details of PCD - MFRC522 Card Reader details
+    Serial.println(F("Scan PICC to see UID, type, and data blocks..."));
+}
+
+void loop() {
+    // Look for new cards
+    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+        return;
+    }
+
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial()) {
+        return;
+    }
+
+    // Dump debug info about the card; PICC_HaltA() is automatically called
+    mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+}
+
+void ShowReaderDetails() {
+    // Get the MFRC522 software version
+    byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
+    Serial.print(F("MFRC522 Software Version: 0x"));
+    Serial.print(v, HEX);
+    if (v == 0x91)
+        Serial.print(F(" = v1.0"));
+    else if (v == 0x92)
+        Serial.print(F(" = v2.0"));
+    else
+        Serial.print(F(" (unknown)"));
+    Serial.println("");
+    // When 0x00 or 0xFF is returned, communication probably failed
+    if ((v == 0x00) || (v == 0xFF)) {
+        Serial.println(F("WARNING: Communication failure, is the MFRC522 properly connected?"));
+    }
+}
